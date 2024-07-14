256908ffb88a8f85c77f1797404404d0c545a368
ViniciusSchmitzMartins <viniciusschmitz2003@gmail.com>
2024-07-14 13:16:28 -0300

    King moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,42 +11,39 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessmatch = new ChessMatch();
-		
-		while(true) {
+
+		while (true) {
 			try {
 				UI.clearScreen();
 				UI.printBoard(chessmatch.getPieces());
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+
+				boolean[][] possibleMoves = chessmatch.possibleMoves(source);
 				
+				UI.clearScreen();
+				UI.printBoard(chessmatch.getPieces(), possibleMoves);
+
 				System.out.println();
-				
+
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessmatch.performChessMove(source, target);
-		
-			}
-			catch (ChessException e) {
+
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			
-			}
-		
-		
-		
-		
-		
-		
+
+		}
 
 	}
 
